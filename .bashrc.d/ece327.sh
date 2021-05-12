32ed97a3ed5497b0cdebd8c3d6b0490450085d1d
Alain Lou <az2lou@uwaterloo.ca>
2021-05-11 22:05:31 -0400

    Update ece327.sh

--- .bashrc.d/ece327.sh
+++ .bashrc.d/ece327.sh
@@ -1,3 +1,3 @@
 alias create_ece327="xhost + && sudo setenforce 0 && podman run -ti --name ece327container --net=host -e DISPLAY=${DISPLAY} -v /tmp/.X11-unix/:/tmp/.X11-unix -v ${HOME}:/mnt nachiketkapre/ece327:sim-impl"
-alias open_ece327="podman start -ia ece327container"
-alias delete_ece327="podman rm ece327container"
+alias open_ece327="xhost + && sudo setenforce 0 && podman start -ia ece327container"
+alias delete_ece327="xhost + && sudo setenforce 0 && podman rm ece327container"
