3f6e4c996329ce88d04fdf56392757594751eb0b
Alain Lou <alainzlou@gmail.com>
2023-06-23 04:50:49 -0400

    Create scan_pdf.sh

---
+++ .bashrc.d/scan_pdf.sh
@@ -0,0 +1,4 @@
+function scan_pdf()
+{
+  convert -density 150 $1 -colorspace gray -linear-stretch 3.5%x10% -blur 0x0.5 -attenuate 0.25 +noise Gaussian -rotate 0.5 temp.pdf && gs -dSAFER -dBATCH -dNOPAUSE -dNOCACHE -sDEVICE=pdfwrite -sColorConversionStrategy=LeaveColorUnchanged -dAutoFilterColorImages=true -dAutoFilterGrayImages=true -dDownsampleMonoImages=true -dDownsampleGrayImages=true -dDownsampleColorImages=true -sOutputFile=output.pdf temp.pdf
+}
